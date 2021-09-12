5e8540465636f11dfcabf46a22d5b00763cf6b54
semensanyok <semenkomissarov@gmail.com>
2021-09-13 01:39:32 +0300

    fix submodules

---
+++ add_submodules.py
@@ -0,0 +1,14 @@
+
+import subprocess
+for path, url in [
+    ("engine/dependencies/assimp", "https://github.com/assimp/assimp.git"),
+    ("engine/dependencies/bullet3", "https://github.com/bulletphysics/bullet3.git"),
+    ("engine/dependencies/glad", "https://github.com/Dav1dde/glad.git"),
+    ("engine/dependencies/assimp", "https://github.com/assimp/assimp.git"),
+    ("engine/dependencies/glm", "https://github.com/g-truc/glm.git"),
+    ("engine/dependencies/stb", "https://github.com/nothings/stb.git"),
+    ("engine/dependencies/zlib", "https://github.com/madler/zlib.git"),
+    ("engine/dependencies/freetype", "https://github.com/freetype/freetype.git"),
+    ("engine/dependencies/libRocket", "https://github.com/libRocket/libRocket.git"),
+    ("engine/dependencies/imgui", "https://github.com/ocornut/imgui.git")]:
+    subprocess.run(args=["git", "submodule", "add", url, path])
